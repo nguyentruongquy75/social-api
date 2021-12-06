b1777feef49999314d747acc05904ee9354bb4c2
Truong Quy <nguyentruongquy75@gmail.com>
2021-12-06 15:45:25 +0700

    Fix get user route

--- routers/user.js
+++ routers/user.js
@@ -23,7 +23,7 @@ router.get("/:id", async (req, res) => {
   const id = req.params.id;
   try {
     const user = await User.findById(id).populate({
-      path: " friends",
+      path: "friends",
     });
     res.status(200).json(user);
   } catch (error) {
